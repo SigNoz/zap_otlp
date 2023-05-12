327e8d34f4a24a33e7b17a22c75cf67beb73dce9
nityanandagohain <nityanandagohain@gmail.com>
2023-05-12 17:29:28 +0530

    feat: inital test for otlp encoder

---
+++ zap_otlp_encoder/zap_otlp_encoder_test.go
@@ -0,0 +1,134 @@
+package zap_otlp_encoder
+
+import (
+	"fmt"
+	"testing"
+	"time"
+
+	. "github.com/smartystreets/goconvey/convey"
+	cv1 "go.opentelemetry.io/proto/otlp/common/v1"
+	lv1 "go.opentelemetry.io/proto/otlp/logs/v1"
+	"google.golang.org/protobuf/proto"
+
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+func TestOTLPEncodeEntry(t *testing.T) {
+	type bar struct {
+		Key string  `json:"key"`
+		Val float64 `json:"val"`
+	}
+
+	type foo struct {
+		A string  `json:"aee"`
+		B int     `json:"bee"`
+		C float64 `json:"cee"`
+		D []bar   `json:"dee"`
+	}
+
+	tests := []struct {
+		name     string
+		desc     string
+		expected *lv1.LogRecord
+		ent      zapcore.Entry
+		fields   []zapcore.Field
+	}{
+		{
+			name: "Test 1",
+			desc: "entry with just body",
+			expected: &lv1.LogRecord{
+				TimeUnixNano:   1529426022000000099,
+				SeverityNumber: lv1.SeverityNumber_SEVERITY_NUMBER_INFO,
+				SeverityText:   "info",
+				Body:           &cv1.AnyValue{Value: &cv1.AnyValue_StringValue{StringValue: "lob law"}},
+			},
+			ent: zapcore.Entry{
+				Level:   zapcore.InfoLevel,
+				Time:    time.Date(2018, 6, 19, 16, 33, 42, 99, time.UTC),
+				Message: "lob law",
+			},
+		},
+		{
+			name: "Test 2",
+			desc: "Add logger name",
+			expected: &lv1.LogRecord{
+				TimeUnixNano:   1529426022000000099,
+				SeverityNumber: lv1.SeverityNumber_SEVERITY_NUMBER_INFO,
+				SeverityText:   "info",
+				Body:           &cv1.AnyValue{Value: &cv1.AnyValue_StringValue{StringValue: "lob law"}},
+				Attributes: []*cv1.KeyValue{
+					{Key: "logger", Value: &cv1.AnyValue{Value: &cv1.AnyValue_StringValue{StringValue: "bob"}}},
+				},
+			},
+			ent: zapcore.Entry{
+				Level:      zapcore.InfoLevel,
+				Time:       time.Date(2018, 6, 19, 16, 33, 42, 99, time.UTC),
+				LoggerName: "bob",
+				Message:    "lob law",
+			},
+		},
+		{
+			name: "Test 3",
+			desc: "info entry with some fields",
+			expected: &lv1.LogRecord{
+				TimeUnixNano:   1529426022000000099,
+				SeverityNumber: lv1.SeverityNumber_SEVERITY_NUMBER_INFO,
+				SeverityText:   "info",
+				Body:           &cv1.AnyValue{Value: &cv1.AnyValue_StringValue{StringValue: "lob law"}},
+				Attributes: []*cv1.KeyValue{
+					{Key: "logger", Value: &cv1.AnyValue{Value: &cv1.AnyValue_StringValue{StringValue: "bob"}}},
+					{Key: "so", Value: &cv1.AnyValue{Value: &cv1.AnyValue_StringValue{StringValue: "passes"}}},
+					{Key: "answer", Value: &cv1.AnyValue{Value: &cv1.AnyValue_IntValue{IntValue: 42}}},
+					{Key: "common_pie", Value: &cv1.AnyValue{Value: &cv1.AnyValue_DoubleValue{DoubleValue: 3.14}}},
+					{Key: "a_float32", Value: &cv1.AnyValue{Value: &cv1.AnyValue_DoubleValue{DoubleValue: 2.7100000381469727}}},
+
+					// TODO: complex/array/object support to be added
+					// {Key: "complex_value", Value: &cv1.AnyValue{Value: &cv1.AnyValue_StringValue{StringValue: "3.14-2.71i"}}},
+				},
+			},
+			ent: zapcore.Entry{
+				Level:      zapcore.InfoLevel,
+				Time:       time.Date(2018, 6, 19, 16, 33, 42, 99, time.UTC),
+				LoggerName: "bob",
+				Message:    "lob law",
+			},
+			fields: []zapcore.Field{
+				zap.String("so", "passes"),
+				zap.Int("answer", 42),
+				zap.Float64("common_pie", 3.14),
+				zap.Float32("a_float32", 2.71),
+				zap.Complex128("complex_value", 3.14-2.71i), // currently ignored by the encoder
+				zap.Reflect("such", foo{ // currently ignored by the encoder
+					A: "lol",
+					B: 123,
+					C: 0.9999,
+					D: []bar{
+						{"pi", 3.141592653589793},
+						{"tau", 6.283185307179586},
+					},
+				}),
+			},
+		},
+	}
+
+	enc := NewOTLPEncoder(zap.NewProductionEncoderConfig())
+
+	for _, tt := range tests {
+		Convey(tt.name, t, func() {
+			buf, err := enc.EncodeEntry(tt.ent, tt.fields)
+			So(err, ShouldBeNil)
+
+			// For debugging purpose uncomment the lines below
+			r := &lv1.LogRecord{}
+			err = proto.Unmarshal([]byte(buf.String()), r)
+			So(err, ShouldBeNil)
+			fmt.Println(r)
+
+			d, err := proto.Marshal(tt.expected)
+			So(err, ShouldBeNil)
+			So(d, ShouldResemble, []byte(buf.String()))
+			buf.Free()
+		})
+	}
+}
